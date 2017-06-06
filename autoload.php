1b66c41bfde21a3c90369d6b3d3adf5b2bd3b4b2
Vlatko Koudela <vlatko@koudela.org>
2017-06-06 10:52:24 +0200

    Autoloader

--- autoload.php
+++ autoload.php
@@ -4,5 +4,5 @@
  * This file is part of the Koldy package.
  *
  */
-require __DIR__ . '/src/Koldy/Autoloader.php';
-Koldy\Autoloader::register();
+require __DIR__ . '/src/KoldyAdmin/Autoloader.php';
+KoldyAdmin\Autoloader::register();
---
+++ src/KoldyAdmin/Autoloader.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace KoldyAdmin;
+
+class Autoloader
+{
+
+    /**
+     * @var string
+     */
+    private $directory;
+
+    /**
+     * @var string
+     */
+    private $prefix;
+
+    /**
+     * @var int
+     */
+    private $prefixLength;
+
+    /**
+     * @param string $baseDirectory Base directory where the source files are located.
+     */
+    public function __construct(string $baseDirectory = __DIR__)
+    {
+        $this->directory = $baseDirectory;
+        $this->prefix = __NAMESPACE__ . '\\';
+        $this->prefixLength = strlen($this->prefix);
+    }
+
+    /**
+     * Registers the autoloader class with the PHP SPL autoloader.
+     *
+     * @param bool $prepend Prepend the autoloader on the stack instead of appending it.
+     */
+    public static function register(bool $prepend = false): void
+    {
+        spl_autoload_register(array(new self(), 'autoload'), true, $prepend);
+    }
+
+    /**
+     * Loads a class from a file using its fully qualified name.
+     *
+     * @param string $className Fully qualified name of a class.
+     */
+    public function autoload(string $className): void
+    {
+        if (0 === strpos($className, $this->prefix)) {
+            if ($className === 'KoldyAdmin') {
+                require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'KoldyAdmin.php';
+            } else {
+                $parts = explode('\\', substr($className, $this->prefixLength));
+                $filepath = $this->directory . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $parts) . '.php';
+
+                if (is_file($filepath)) {
+                    require $filepath;
+                }
+            }
+        }
+    }
+
+}
